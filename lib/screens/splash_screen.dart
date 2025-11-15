1597a03c52ff06f9b180964a1894370031917182
tmawaro25 <tmawaro25@gmail.com>
2025-11-15 11:20:41 +0000

    feat: Implement role-based navigation in splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:carehub/services/auth_service.dart';
+import 'package:carehub/services/database_service.dart';
 import '../widgets/logo.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -10,13 +12,35 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final AuthService _authService = AuthService();
+  final DatabaseService _databaseService = DatabaseService();
+
   @override
   void initState() {
     super.initState();
-    // Navigate to the login screen after a delay
-    Future.delayed(const Duration(seconds: 3), () {
+    _navigate();
+  }
+
+  void _navigate() async {
+    await Future.delayed(const Duration(seconds: 3));
+    final user = _authService.currentUser;
+    if (user != null) {
+      final userDoc = await _databaseService.users.doc(user.uid).get();
+      if (userDoc.exists) {
+        final userRole = userDoc.get('role');
+        if (userRole == 'parent') {
+          Get.offNamed('/parent_dashboard');
+        } else if (userRole == 'caregiver') {
+          Get.offNamed('/caregiver_dashboard');
+        } else {
+          Get.offNamed('/login');
+        }
+      } else {
+        Get.offNamed('/login');
+      }
+    } else {
       Get.offNamed('/login');
-    });
+    }
   }
 
   @override
@@ -37,6 +61,8 @@ class _SplashScreenState extends State<SplashScreen> {
                 color: Colors.lightGreen[600],
               ),
             ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(),
           ],
         ),
       ),
